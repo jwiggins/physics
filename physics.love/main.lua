d042c9750084f489a72913194a66f0a614dac9f6
John Wiggins <jwiggins@enthought.com>
2014-02-01 00:04:39 -0600

    Get a super basic physics demo working.

--- physics.love/main.lua
+++ physics.love/main.lua
@@ -1,9 +1,37 @@
 
+--local world, boxBody, groundBody
+
 function love.load()
+    love.physics.setMeter(64)
+    world = love.physics.newWorld(0.0, 9.8*64, true)
+
+    local width, height = love.graphics.getDimensions()
+    groundBody = love.physics.newBody(world, width/2, height-30, "static")
+    local shape = love.physics.newRectangleShape(width, 15)
+    local fixture = love.physics.newFixture(groundBody, shape, 0.0)
+    fixture:setRestitution(0.5)
+
+
+    boxBody = love.physics.newBody(world, width/2, 100, "dynamic")
+    local shape = love.physics.newRectangleShape(40, 40)
+    local fixture = love.physics.newFixture(boxBody, shape, 0.0)
+    fixture:setRestitution(0.75)
 end
 
 function love.update(dt)
+    world:update(dt)
 end
 
 function love.draw()
+    local width, height = love.graphics.getDimensions()
+    love.graphics.push()
+    love.graphics.setColor(255, 0, 255, 128)
+    local x, y = groundBody:getPosition()
+    love.graphics.rectangle("fill", x-width/2, y-7.5, width, 15)
+
+    x, y = boxBody:getPosition()
+    love.graphics.push()
+    love.graphics.rectangle("fill", x-20, y-20, 40, 40)
+    love.graphics.pop()
+    love.graphics.pop()
 end
