58d642f1c74fdfb2f682846d408118bc4e9c05bc
John Wiggins <jwiggins@enthought.com>
2014-02-03 22:09:03 -0600

    Tweak the pixels to meters conversion and fix the time step.
    The python version now closely matches the Lua version!

--- python/main.py
+++ python/main.py
@@ -2,16 +2,16 @@ import math
 from random import randint
 import sys
 
-from Box2D import b2BodyDef, b2FixtureDef, b2Shape, b2World
+from Box2D import b2Shape, b2World
 from pygame import init, display, draw, event, locals, Color
 from pygame.time import Clock
 
 w_width, w_height = 800, 600
 black = Color(0, 0, 0)
-white = Color(255, 255, 255)
 world = b2World(gravity=(0, 9.8))
-actors = []
-colors = []
+actors, colors = [], []
+PPM = 64.0  # Pixels per Meter
+
 
 def add_rect_body(kind, x, y, width, height, angle=0.0, mass=1.0, rest=0.0,
                   frict=0.75):
@@ -20,8 +20,8 @@ def add_rect_body(kind, x, y, width, height, angle=0.0, mass=1.0, rest=0.0,
         'dynamic': world.CreateDynamicBody,
     }[kind]
 
-    body = body_factory(position=(x/64.0, y/64.0), angle=angle, mass=mass)
-    body.CreatePolygonFixture(box=(width/64.0, height/64.0), density=1,
+    body = body_factory(position=(x/PPM, y/PPM), angle=angle, mass=mass)
+    body.CreatePolygonFixture(box=(width/PPM, height/PPM), density=1,
                               friction=frict, restitution=rest)
     return body
 
@@ -32,15 +32,15 @@ def ground_arc(radius, segments, centerX, centerY):
         angle = angle_incr*(i+1)
         x = centerX + math.cos(angle) * radius
         y = centerY + math.sin(angle) * radius
-        add_rect_body("static", x, y, 5, 40, angle)
+        add_rect_body("static", x, y, 2.5, 20, angle)
 
 
 def collision_targets():
     for i in range(9):
         x, y = (200, w_height-20-i*20)
-        body = add_rect_body("dynamic", x, y, 30, 10,
+        body = add_rect_body("dynamic", x, y, 15, 10,
                              angle=0.0, mass=0.01, rest=0.2, frict=0.75)
-        body.userData = (x/64.0, (y-10)/64.0)
+        body.userData = (x/PPM, y/PPM)
         actors.append(body)
 
 
@@ -60,10 +60,10 @@ def load():
     collision_targets()
 
     x, y = w_width-42, 100
-    ball = world.CreateDynamicBody(position=(x/64.0, y/64.0), mass=10.0)
-    ball.CreateCircleFixture(radius=40/64.0, density=1,
+    ball = world.CreateDynamicBody(position=(x/PPM, y/PPM), mass=10.0)
+    ball.CreateCircleFixture(radius=40/PPM, density=1,
                              friction=0.75, restitution=0.5)
-    ball.userData = (x/64.0, y/64.0)
+    ball.userData = (x/PPM, y/PPM)
     actors.append(ball)
 
 
@@ -78,19 +78,19 @@ def render(surface):
     for bv in world:
         x, y = bv.position
         if c >= len(colors):
-            colors.append(Color(randint(20, 255), randint(20, 255),
-                                randint(20, 255)))
+            colors.append(Color(randint(40, 255), randint(40, 255),
+                                randint(40, 255)))
         color = colors[c]
         c += 1
         for fv in bv:
             s = fv.shape
             st = fv.shape.type
             if st == b2Shape.e_circle:
-                draw.circle(surface, color, (int(x*64), int(y*64)),
-                            int(s.radius*64))
+                draw.circle(surface, color, (int(x*PPM), int(y*PPM)),
+                            int(s.radius*PPM))
             elif st == b2Shape.e_polygon:
                 transformed_verts = [bv.transform * v for v in s.vertices]
-                verts = [(v.x*64, v.y*64) for v in transformed_verts]
+                verts = [(v.x*PPM, v.y*PPM) for v in transformed_verts]
                 draw.polygon(surface, color, verts)
 
 
@@ -110,7 +110,7 @@ if __name__ == '__main__':
                 if e.key == ord(' '):
                     reset_bodies()
 
-        dt = fps_clock.tick(30)
+        dt = fps_clock.tick(60)
         update(dt/1000.0)
 
         render(window_surface)
